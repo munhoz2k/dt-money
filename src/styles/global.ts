da6a1d26b20d7cec494446d981ae8f6c40b58702
munhoz2k <lucasmunhozarruda@gmail.com>
2024-02-21 20:48:57 -0300

    Irrelevant Changes

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,13 +1,13 @@
 import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
-  *{
+  * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
   }
 
-  :focus {
+  :focus { 
     outline: 0;
     box-shadow: 0 0 0 1px ${(props) => props.theme['green-500']};
   }
